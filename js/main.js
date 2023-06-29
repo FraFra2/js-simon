85af87c3ac632bb38e8996527dcffe65ad06e611
FraFra2 <kekkino41@gmail.com>
2023-06-29 14:39:30 +0200

    ADD TImer e controllo tra i due array

--- js/main.js
+++ js/main.js
@@ -8,35 +8,48 @@ const generateRandomNumbers = () => {
     
     return numbers;
   };
-
-
+  
   let second = 5;
   let userInputs = [];
-
+  
   const numbersContainer = document.querySelector(".numbers-container");
   const timerContainer = document.querySelector(".timer-container");
-
-
-  numbersContainer.innerHTML = `<h1 class = "text-center">Simon Says: ${generateRandomNumbers()}</h1>`;
-
-    timerContainer.innerHTML = `Tempo rimanente: ${second}`;
-
-  setInterval(() =>{
+  const userNumbersLabel = document.querySelector(".user-numbers");
+  
+  const simonNumbers = generateRandomNumbers();
+  
+  numbersContainer.innerHTML = `<h1 class="text-center">Simon Says: ${simonNumbers}</h1>`;
+  
+  timerContainer.innerHTML = `Tempo rimanente: ${second}`;
+  
+  setInterval(() => {
     second -= 1;
     timerContainer.innerHTML = `Tempo rimanente: ${second}`;
-    if(second < 1){
-        timerContainer.innerHTML = "Tempo Scaduto";
-
+    if (second < 1) {
+      timerContainer.innerHTML = "Tempo Scaduto";
+      numbersContainer.classList.add("d-none");
     }
-}, 1000);   
-
-setTimeout(() =>{
-    numbersContainer.classList.add("d-none");
+  }, 1000);   
+  
+  setTimeout(() => {
     for (let index = 0; index < 5; index++) {
-        let element = userInputs[index];
-        element = prompt(`Inserire l'${index} numero.`);
-        userInputs.push(element);
+      let element = prompt(`Inserire il ${index + 1}° numero.`);
+      userInputs.push(Number(element));
     }
-}, second * 1000);
-
-
+  
+    userNumbersLabel.innerHTML = `
+      <div>
+          <h1>I numeri di Simon: ${simonNumbers}</h1>
+          <h1>I numeri da te inseriti: ${userInputs}</h1>
+      </div>
+    `;
+  
+    const compareArrays = (array1, array2) => {
+      const resultArray = array1.filter((element) => array2.includes(element));
+      return resultArray;
+    };
+  
+    let rightNums = compareArrays(simonNumbers, userInputs);
+    userNumbersLabel.innerHTML += `Numeri corretti inseriti: ${rightNums}`; 
+  }, second * 1000);
+  
